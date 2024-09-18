4eb3b8b5998862d6c09663a1401666291a2e5f3c
Denlavor <rafinhacrisdenlavor@gmail.com>
2024-09-18 18:38:07 -0300

    fix: renaming import

--- src/routes/get-week-summary.ts
+++ src/routes/get-week-summary.ts
@@ -1,5 +1,5 @@
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
-import { getWeekSummary } from "../features/get-week-sumarry";
+import { getWeekSummary } from "../features/get-week-summary";
 import { z } from "zod";
 
 export const getSummaryRoute: FastifyPluginAsyncZod = async (app, _options) => {
