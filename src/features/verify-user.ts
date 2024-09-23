a3e4dab1c5bfff3ab251f41dec92c0631c002fc5
Denlavor <rafinhacrisdenlavor@gmail.com>
2024-09-23 18:09:06 -0300

    bugfix: avoiding login only with valid username

--- src/features/verify-user.ts
+++ src/features/verify-user.ts
@@ -1,9 +1,14 @@
 import dayjs from "dayjs";
-import { eq } from "drizzle-orm";
+import { eq, and } from "drizzle-orm";
 import { db } from "../db";
 import { users } from "../db/schema";
 export async function verifyUser(username : string, password: string) {
-  const user = await db.select().from(users).where(eq(users.username, username), eq(users.password, password));
+  const user = await db.select().from(users).where(
+    and(
+      eq(users.username, username),
+      eq(users.password, password)
+    )
+  );
 
   if(user.length === 0) {
     return { hasUser: false }
