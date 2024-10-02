326fc7bc759e29ebfbf0474010fce9ff04db9fec
Denlavor <rafinhacrisdenlavor@gmail.com>
2024-10-02 17:31:25 -0300

    fix: preventing the transfer of sensitive user data

--- src/features/get-user.ts
+++ src/features/get-user.ts
@@ -3,6 +3,12 @@ import { eq } from "drizzle-orm";
 import { db } from "../db";
 import { users } from "../db/schema";
 export async function getUser(userId: string) {
-  const user = await db.select().from(users).where(eq(users.id, userId));
+  const user = await db.select({
+    name: users.name,
+    email: users.email
+    avatar: users.avatar,
+    motivationalPhrase: users.motivationalPhrase,
+    createdAt: users.createdAt
+  }).from(users).where(eq(users.id, userId));
   return user;
 }
